9b4c2eb93bf9014fc3d9e97a752ad2e3747a3887
prathameshpm <mahamulkarprathamesh@gmail.com>
2017-09-19 13:58:06 +0530

    Wired up reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+  return [
+    {title: 'Javascript: The good Parts'},
+    {title: 'My experiments with my truths'},
+    {title: 'Eloquent Ruby'},
+    {title: 'Wings of Fire'},
+    {title: 'India after Independence'}
+  ]
+}
